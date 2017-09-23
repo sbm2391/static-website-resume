97e56b10947f39f15a7f232656720843284b5fdc
Bet M <sbm2391@hotmail.com>
2017-09-22 17:44:25 -0700

    click event experiencia-educacion-cursos

--- main.js
+++ main.js
@@ -1,19 +1,68 @@
-var button = document.getElementById("toggle");
-var target = document.getElementById("target");
+/*experiencia*/
+var button = document.getElementById("toggleex");
+var target = document.getElementById("targetex");
 var bool = true;
+
 
 function displayToggle() {
     if (bool) {
         target.setAttribute("class", "experiencia show")
         bool = false;
+        button.setAttribute("class", "selectorex azul")
+
     } else {
         target.setAttribute("class", "hide")
         bool = true;
+        button.setAttribute("class", "selectorex gris")
     }
 }
 
 button.addEventListener("click", displayToggle, bool);
 
+/*educacion*/
+var buttonedu = document.getElementById("toggleedu");
+var targetedu = document.getElementById("targetedu");
+var bool = true;
+
+
+function displayToggleEdu() {
+    if (bool) {
+        targetedu.setAttribute("class", "educacion show")
+        bool = false;
+        buttonedu.setAttribute("class", "selectoredu azul")
+
+    } else {
+        targetedu.setAttribute("class", "hide")
+        bool = true;
+        buttonedu.setAttribute("class", "selectoredu gris")
+    }
+}
+
+buttonedu.addEventListener("click", displayToggleEdu, bool);
+
+/*cursos*/
+var buttoncur = document.getElementById("togglecur");
+var targetecur = document.getElementById("targetecur");
+var bool = true;
+
+
+function displayToggleCur() {
+    if (bool) {
+        targetecur.setAttribute("class", "cursos show")
+        bool = false;
+        buttoncur.setAttribute("class", "selectorcur azul")
+
+    } else {
+        targetecur.setAttribute("class", "hide")
+        bool = true;
+        buttoncur.setAttribute("class", "selectorcur gris")
+    }
+
+}
+
+buttoncur.addEventListener("click", displayToggleCur, bool);
+
+/*menu*/
 var btn = document.getElementById("btn");
 var menu = document.getElementById("mininav");
 
